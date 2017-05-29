a810f199817ba3bfc7118012f3a49f6c42bde578
Tibau <xiliar1888@hotmail.fr>
2017-05-29 11:37:24 +0200

    Init of Django,add bower, add bootstrap, add sass, add project people, add urls, add templates, add some style scss for testing sass and some div for bootstrap

---
+++ reseau_social/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import url, include
+from django.contrib import admin
+from django.contrib.auth import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+urlpatterns = [
+    url(r'^', include("people.urls")),
+    url(r'^admin/', admin.site.urls),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
